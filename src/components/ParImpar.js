9b44f383c54cddd9b53220e89b0c488296005cf5
ReinanHS <reinangabriel1520@gmail.com>
2019-12-26 22:52:07 -0300

    renderização de condicional método 2

--- src/components/ParImpar.js
+++ src/components/ParImpar.js
@@ -2,10 +2,15 @@ import React from 'react'
 import { View, Text, TextInput } from 'react-native'
 import StyleMain from '../styles/StyleMain'
 
+function ParOrImpar(numero){
+    const result = numero % 0 == 0 ? 'Par' : 'Impar'
+    return <Text style={[StyleMain.ex]}>O número { numero } é {result}</Text>
+}
+
 export default (props) => {
     return (
         <View>
-            { props.numero % 2 == 0 ? <Text style={[StyleMain.ex]}>O número é Par</Text> : <Text style={[StyleMain.ex]}>O número é Impar</Text>  }
+            { ParOrImpar(props.numero) }
         </View>
     )
 }
